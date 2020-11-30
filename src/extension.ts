c124948289d6a737a9b45bb3675b87e0264fcbb3
CareteenL <15074806497@163.com>
2020-11-30 17:56:50 +0800

    feat: 🎸 support .i(next line), .r(replace), .t(wrap time)

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,65 @@
+/**
+ * @desc 为所选择代码段添加`log`
+ * @usage 1. obj.i 插入到下一行
+ * 			  2. obj.r 替换当前选中字符
+ * 			  3. obj.t 打印代码耗时
+ */
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "ext-insert_log" is now active!');
 
-	const insertText = (log: string) => {
+	// 插入到下一行
+	const insertLog = (text: string) => {
 		const editor = vscode.window.activeTextEditor;
-		const selection = editor?.selection;
+		const selection = editor?.selection;		
+		const logToInsert = text ? `console.log('${text}: ', ${text});` : `console.log();`;
 		const lineOfSelectedVar = selection?.active.line;
-		console.log(selection);
 		editor?.edit((editBuilder: vscode.TextEditorEdit) => {
-			// 插入到下一行
-			// editBuilder.insert(new vscode.Position(lineOfSelectedVar! + 1, 0), `${log}\n`);
-			// 替换当前选中字符
-			editBuilder.replace(new vscode.Range(selection?.start!, selection?.end!), log);
+			editBuilder.insert(new vscode.Position(lineOfSelectedVar! + 1, 0), `${logToInsert}\n`);
+		});
+	};
+
+	// 替换当前选中字符
+	const replaceLog = (text: string) => {
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor?.selection;		
+		const logToReplace = text ? `console.log('${text}: ', ${text});` : `console.log();`;
+		editor?.edit((editBuilder: vscode.TextEditorEdit) => {
+			editBuilder.replace(new vscode.Range(selection?.start!, selection?.end!), logToReplace);
+		});
+	};
+
+	// 打印代码耗时
+	const wrapTime = (text: string) => {
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor?.selection;		
+		if (!text) {
+			return;
+		};
+		const codeTowrap = `console.time('timer');\n${text}\nconsole.timeEnd('timer');\n`;
+		editor?.edit((editBuilder: vscode.TextEditorEdit) => {
+			editBuilder.replace(new vscode.Range(selection?.start!, selection?.end!), codeTowrap);
 		});
 	};
 
 	let disposable = vscode.commands.registerCommand('ext-insert_log.insertLog', () => {
-
 		const editor = vscode.window.activeTextEditor;
 		const selection = editor?.selection;
-		const text = editor?.document.getText(selection);
-		const logToInsert = `console.log('${text}: ', ${text});`;
-		text ? insertText(logToInsert) : insertText(`console.log();`);
+		const text = editor?.document.getText(selection).trim();
+		if (/\.i$/.test(text!)) {
+			insertLog(text!.slice(0, -2));
+		} else if (/\.r$/.test(text!)) {
+			replaceLog(text!.slice(0, -2));
+		} else if (/\.t$/.test(text!)) {
+			wrapTime(text!.slice(0, -2));
+		} else {
+			insertLog(text!.slice(0, -2));
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
